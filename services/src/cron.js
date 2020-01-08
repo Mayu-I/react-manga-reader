fea88adfae6b10d0096dbb51e446788f141fa2fb
Mayu Imanaka <imamayu.1215@gmail.com>
2020-01-08 13:04:34 -0800

    add:manga list to mongodb

--- services/src/cron.js
+++ services/src/cron.js
@@ -1,51 +1,51 @@
-// import axios from "axios";
-// import "dotenv/config";
-// import cron from "node-cron";
-// import "#root/db/connection";
-// import Manga from "#root/db/models/Manga";
+import axios from "axios";
+import "dotenv/config";
+import cron from "node-cron";
+import "#root/db/connection";
+import Manga from "#root/db/models/Manga";
 
-// const axiosME = axios.create({
-//     baseURL: process.env.MANGA_EDEN_URL,
-// });
+const axiosME = axios.create({
+    baseURL: process.env.MANGA_EDEN_URL,
+});
 
-// const transformMangaEden = manga =>
-//     manga
-//         .filter(manga => manga.ld)
-//         .map(
-//             ({
-//                 a: alias,
-//                 c: categories,
-//                 h: hits,
-//                 i: _id,
-//                 im: image,
-//                 ld: lastUpdated,
-//                 s: status,
-//                 t: title
-//             }) => ({
-//                 _id,
-//                 alias,
-//                 categories,
-//                 hits,
-//                 image,
-//                 lastUpdated,
-//                 status,
-//                 title
-//             })
-//         );
+const transformMangaEden = manga =>
+    manga
+        .filter(manga => manga.ld)
+        .map(
+            ({
+                a: alias,
+                c: categories,
+                h: hits,
+                i: _id,
+                im: image,
+                ld: lastUpdated,
+                s: status,
+                t: title
+            }) => ({
+                _id,
+                alias,
+                categories,
+                hits,
+                image,
+                lastUpdated,
+                status,
+                title
+            })
+        );
 
-// const seed = async () => {
-//     const res = await axiosME.get();
-//     const mangas = transformMangaEden(res.data.manga);
+const seed = async () => {
+    const res = await axiosME.get();
+    const mangas = transformMangaEden(res.data.manga);
 
-//     console.log(`mangas back, got ${mangas.length}`);
+    console.log(`mangas back, got ${mangas.length}`);
 
-//     await Manga.insertMany(mangas);
+    await Manga.insertMany(mangas);
 
-//     console.log("seeded");
-// };
+    console.log("seeded");
+};
 
-// seed();
+seed();
 
-// cron.schedule("0 * * * *", () => {
-//     console.log("runnning a task every hour");
-// })
+cron.schedule("0 * * * *", () => {
+    console.log("runnning a task every hour");
+})
--- services/src/index.js
+++ services/src/index.js
@@ -1,21 +1,2 @@
-import "dotenv/config";
-
-import "./db/connection";
+import "dotenv/config"
 import "./server";
-
-import Manga from "#root/db/models/Manga";
-
-const manga = new Manga({
-    _id: "5db37427719a168daca60ff5",
-    alias: "haunted",
-    categories: [],
-    hits: 0,
-    image: null,
-    status: 1,
-    title: "HAUNTED (ホーンテッド)"
-});
-
-manga.save(function (err, manga) {
-    if (err) return console.error(err);
-    console.log("saved");
-})
